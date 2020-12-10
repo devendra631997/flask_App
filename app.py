973e114dfc8de64601fce9e29999ee324da9d7ed
devendra <it.programmer@saraffglobal.com>
2020-12-10 17:27:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+# from flask import Flask, jsonify, request, make_response, render_template, redirect, url_for
+from flask import *  
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import jwt 
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
+
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') #http://127.0.0.1:5000/route?token=alshfjfjdklsfj89549834ur
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 403
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 403
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/uploadImage')
+@limiter.limit("5 per minute")
+@token_required
+def image_upload():
+    return  render_template('uploadImage.html')
+
+@app.route('/success', methods = ['POST'])
+@limiter.limit("5 per minute")  
+@token_required
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return render_template("uploaded.html", name = f.filename)  
+
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({'message' : 'Anyone can view this!'})
+
+@app.route('/protected')
+@limiter.limit("5 per minute")
+@token_required
+def protected():
+    return jsonify({'message' : 'This is only available for people with valid tokens.'})
+
+@app.route('/')
+@limiter.limit("5 per minute")
+def login():
+    auth = request.authorization
+    print(auth)
+    if auth and auth.password == 'secret':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=15)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
